785f136fa62ef6d29814a04066efc11370a705a1
Eugene Choi <eugene.maestro.choi@gmail.com>
2014-11-04 18:36:04 +0800

    minor css fixes on navigation

--- client/app/components/nav/nav.js
+++ client/app/components/nav/nav.js
@@ -1,13 +1,34 @@
 /** @jsx React.DOM */
 
+// var NavigationButton = React.createClass({
+//   getInitialState: function () {
+//     return {active: false};
+//   },
+
+//   clicked: function (index) {
+//     this.setState({active: index}, function(){
+//       console.log(index, ' clicked, ', this.state);
+//     });
+//   },
+  
+//   render: function () {
+//     return (
+//       <div className="nav">
+//         <a className="nav-logo">LOGO</a>
+//         {buttonsObj}
+//       </div>
+//     );
+//   }
+// });
+
 var NavigationBar = React.createClass({
   getInitialState: function () {
     return {active: false};
   },
+
   clicked: function (index) {
-    this.setState({active: index}, function(){
-      console.log(index, ' clicked, ', this.state);
-    });
+    this.setState({active: index});
+    console.log('clicked ', index);
   },
 
   render: function() {
@@ -16,13 +37,15 @@ var NavigationBar = React.createClass({
 
     this.props.buttons.map(function (item, index) {
       var buttonClasses = 'nav-button'
+      // var item = item.main;
       
       if (self.state.active === index) {
         buttonClasses += ' active';
-        console.log('classes', buttonClasses);
       }
 
-      buttonsObj['result-' + item] = <a className={buttonClasses} onClick={self.clicked.bind(self, index)}>{item}</a>;
+      buttonsObj['result-' + item] = <a className={buttonClasses} onClick={self.clicked.bind(self, index)}>
+        {item}
+        </a>
     });
 
     return (
@@ -34,5 +57,18 @@ var NavigationBar = React.createClass({
   }
 });
 
+
+// var navigationButtons = [{
+//   main: 'one',
+//   dropdown: ['one-one', 'one-two']
+// },
+// {
+//   main: 'two',
+//   dropdown: ['two-one', 'two-two']
+// },
+// {
+//   main: 'three',
+//   dropdown: ['three-one', 'three-two', 'three-three']
+// }];
 
 React.render(<NavigationBar buttons={['one', 'two', 'three']}/>, document.getElementById('content'));
